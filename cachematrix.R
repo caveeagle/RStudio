cc9e453fae44c52693f2634335c0a750ba7a59c2
caveeagle <a@a.ru>
2015-03-11 19:03:45 +0300

    1

---
+++ cachematrix.R
@@ -0,0 +1,64 @@
+
+## Caching the Inverse of a Matrix
+
+makeCacheMatrix <- function(x = matrix()) 
+{
+    INV <- NULL # storage for inverse of the matrix
+    
+    set <- function(y) { # set the value of the matrix
+        x <<- y
+        inv <<- NULL
+    }
+    get <- function() { x } # get the value of the matrix
+    
+    # set the value of inverse of the matrix
+    setInverse <- function(inverse) { INV <<- inverse  }
+    
+    # get the value of inverse of the matrix
+    getInverse <- function() { INV }
+    
+    # return a list of functions
+    list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
+}
+
+
+## function returns the inverse of the matrix
+
+cacheSolve <- function(x, ...) 
+{
+    INV <- x$getInverse()
+    
+    if(!is.null(INV)) # if cashed value exists
+    {
+        message("getting cached data")
+        return(INV)
+    }
+    data <- x$get()
+    INV <- solve(data)
+    x$setInverse(INV)
+    INV
+}
+
+############################
+############################
+
+## Check the solution:
+
+## Take the matrix 4 by 4, that have a simple solution:
+#v1 = c(1,3,-5,7)
+#v2 = c(0,1,2,-3)
+#v3 = c(0,0,1,2)
+#v4 = c(0,0,0,1)
+#M = rbind(v1,v2,v3,v4)
+
+#mc = makeCacheMatrix(M)
+#mc$get()
+
+## First run (without cache)
+#cacheSolve(mc)
+
+## Second run (with cache)
+#cacheSolve(mc)
+
+
+
